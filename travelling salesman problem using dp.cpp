ac743c33cd9e6cfea40eee1a24d2473f04549add
AASHISH SINGH <allenite@AASHISHs-MacBook-Pro.local>
2019-07-30 15:02:39 +0530

    remove examples

--- travelling salesman problem using dp.cpp
+++ travelling salesman problem using dp.cpp
@@ -3,7 +3,7 @@ using namespace std;
 void print(int n,map<pair<int ,pair<vector<int >, int > >, int >dp)
 {
     cout<<"\n";
-    for(int i=0;i<=n-1;i++)
+    for(int i = 0; i <= n - 1; i++)
     {
         for(map<pair<int ,pair<vector<int >, int > >, int > :: iterator itr = dp.begin(); itr != dp.end(); itr++)
         {
@@ -52,7 +52,6 @@ int TSP(int a, vector<int >v, int **cost, map<pair<int ,pair<vector<int >, int >
                     if(dp[{a, {v, 0}}] > ans)
                     {
                         path[{a, {v, 0}}] = i;
-                        //cout<<a<<" "<<v[i]<<"\n";
                         dp[{a, {v, 0}}] = ans;
                     }
 
@@ -136,24 +135,4 @@ ans = 40
 8 8 9 0
 ans = 35
 
-
-0 2 6
-2 0 0
-0 5 0
-
-0 2 6
-0 0 0
-0 5 0
-3
-3
-0 1 1
-1 2 2
-2 1 3
-
-3
-4
-0 1 2
-0 2 6
-1 0 2
-2 1 5
 */
